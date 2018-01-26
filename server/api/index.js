05960104221b5d537670fa53d96b4dce283d8a17
Jacquelyn Wax <jacquelynwax@gmail.com>
2018-01-26 14:04:25 -0500

    successful test for basic get to a single conversation by id with all associated watchWordOccurrences and snippets

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/watchWords', require('./watchWords'))
+router.use('/conversations', require('./conversations'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
