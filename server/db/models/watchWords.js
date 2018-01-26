3e9777251b6d4bf4323de55791ecee22a4505362
Jacquelyn Wax <jacquelynwax@gmail.com>
2018-01-26 11:15:22 -0500

    complete watchWords model

--- server/db/models/watchWords.js
+++ server/db/models/watchWords.js
@@ -1,2 +1,12 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+module.exports = WatchWords;
+
+const WatchWords = db.define('watchWords', {
+  wordOrPhrase: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  }
+})
