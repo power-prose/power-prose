c1b5ae7ba41b942df7da55876047d472f44235c4
Jacquelyn Wax <jacquelynwax@gmail.com>
2018-01-26 11:09:25 -0500

    seeded file with users

--- script/seed.js
+++ script/seed.js
@@ -1,51 +1,55 @@
-/**
- * Welcome to the seed file! This seed file uses a newer language feature called...
- *
- *                  -=-= ASYNC...AWAIT -=-=
- *
- * Async-await is a joy to use! Read more about it in the MDN docs:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
- *
- * Now that you've got the main idea, check it out in practice below!
- */
-const db = require('../server/db')
-const {User} = require('../server/db/models')
+const { User, db } = require('../server/db/models')
 
-async function seed () {
-  await db.sync({force: true})
-  console.log('db synced!')
-  // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
-  // executed until that promise resolves!
+const users = [{
+  email: '12345@gmail.com',
+  password: 'testpassword1',
+  salt: 'not_sure?',
+  googleId: '12345',
+  firstName: 'Jacquelyn',
+  lastName: 'Wax'
+}, {
+  email: '23456@gmail.com',
+  password: 'testpassword2',
+  salt: 'not_sure?',
+  googleId: '23455',
+  firstName: 'Hannah',
+  lastName: 'Weber'
+}, {
+  email: '34567@gmail.com',
+  password: 'testpassword3',
+  salt: 'not_sure?',
+  googleId: '34567',
+  firstName: 'Ann',
+  lastName: 'Layman'
+} , {
+  email: '45678@gmail.com',
+  password: 'testpassword4',
+  salt: 'not_sure?',
+  googleId: '45678',
+  firstName: 'Lyssa',
+  lastName: 'Stiller'
+}];
 
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
-  // Wowzers! We can even `await` on the right-hand side of the assignment operator
-  // and store the result that the promise resolves to in a variable! This is nice!
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
-}
+const seed = () =>
+  Promise.all(users.map(user =>
+    User.create(user))
+  );
 
-// Execute the `seed` function
-// `Async` functions always return a promise, so we can use `catch` to handle any errors
-// that might occur inside of `seed`
-seed()
-  .catch(err => {
-    console.error(err.message)
-    console.error(err.stack)
-    process.exitCode = 1
-  })
-  .then(() => {
-    console.log('closing db connection')
-    db.close()
-    console.log('db connection closed')
-  })
+const main = () => {
+  console.log('Syncing db...');
+  db.sync({force: true})
+    .then(() => {
+      console.log('Seeding databse...');
+      return seed();
+    })
+    .catch(err => {
+      console.log('Error while seeding');
+      console.log(err.stack);
+    })
+    .then(() => {
+      db.close();
+      return null;
+    });
+};
 
-/*
- * note: everything outside of the async function is totally synchronous
- * The console.log below will occur before any of the logs that occur inside
- * of the async function
- */
-console.log('seeding...')
+main();
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,9 @@
 const User = require('./user')
+const db = require('../db.js')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+// model associations
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+// all models exported here so that any time a module needs a model, we can require it from 'db/models'
 module.exports = {
-  User
+  User, db
 }
