cb43fd39cc740fda9de1e5622093bce1f0709d05
Ann Layman <annelayman@gmail.com>
2018-01-30 14:02:43 -0500

    including accurate time to save in db

--- client/components/RecordButtons.js
+++ client/components/RecordButtons.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { postNewConvo, setRecordedText } from "../store";
+import { postNewConvo, setRecordedText, setConvoStartTime, setConvoEndTime } from "../store";
 const WatsonSpeech = require("watson-speech");
 const axios = require("axios");
 
@@ -11,7 +11,6 @@ class RecordButtons extends Component {
     super(props);
     this.state = {
       text: "",
-      tones: [],
       preSubmit: false
     };
     this.handleStart = this.handleStart.bind(this);
@@ -23,6 +22,8 @@ class RecordButtons extends Component {
   }
 
   handleStart() {
+    const startTime = new Date();
+    this.props.dispatchStartTime(startTime);
     const handleUpdate = this.handleUpdate;
     axios
       .get("/api/speech-to-text/token")
@@ -56,6 +57,9 @@ class RecordButtons extends Component {
   }
 
   handleStop() {
+    const endTime = new Date();
+    this.props.dispatchEndTime(endTime)
+
     this.stream.stop = this.stream.stop.bind(this.stream);
     this.stream.stop();
     this.setState({ preSubmit: true })
@@ -105,15 +109,22 @@ const mapDispatch = (dispatch) => {
   return {
     handleSubmit(event) {
       event.preventDefault();
+
       const conversationData = {
         name: event.target.recordingName.value,
-        lengthTime: "300",
+        //lengthTime: "300",
         //eventually pass in time
       }
       dispatch(postNewConvo(conversationData))
     },
     dispatchText(text) {
       dispatch(setRecordedText(text));
+    },
+    dispatchStartTime(startTime) {
+      dispatch(setConvoStartTime(startTime));
+    },
+    dispatchEndTime(endTime) {
+      dispatch(setConvoEndTime(endTime))
     }
   }
 };
--- client/store/allConversations.js
+++ client/store/allConversations.js
@@ -4,21 +4,23 @@ import {fetchChosenConversation} from './chosenConversation';
 
 
 const SET_ALL_CONVERSATIONS = "SET_ALL_CONVERSATIONS";
-
 const POST_NEW_CONVERSATION = "POST_NEW_CONVERSATION";
-
 const SET_RECORDED_TEXT = "SET_RECORDED_TEXT"
+const SET_CONVO_START_TIME = "SET_CONVO_START_TIME";
+const SET_CONVO_END_TIME = " SET_CONVO_END_TIME";
 
 const initialConversationState = {
   defaultConversations: [],
-  recordedText: ''
+  recordedText: '',
+  startTime: 0,
+  endTime: 0
 }
 
 const setAllConversations = conversations => ({type: SET_ALL_CONVERSATIONS, conversations});
 const postNewConversation = conversation => ({type: POST_NEW_CONVERSATION, conversation})
 export const setRecordedText = (text) => ({type: SET_RECORDED_TEXT, text})
-
-
+export const setConvoStartTime = (time) => ({type: SET_CONVO_START_TIME, time})
+export const setConvoEndTime = (time) => ({type: SET_CONVO_END_TIME, time})
 
 
 export const fetchAllConversations = (userId) => dispatch =>
@@ -29,6 +31,7 @@ axios.get(`/api/conversations/user/${userId}`)
 export function postNewConvo(conversation) {
   return function thunk(dispatch) {
     conversation.text = store.getState().allConversations.recordedText;
+    conversation.lengthTime = (store.getState().allConversations.endTime - store.getState().allConversations.startTime) / 1000;
     return axios.post(`/api/conversations`, conversation)
         .then(res => res.data)
         .then(newConversation => {
@@ -53,6 +56,12 @@ export default function (state = initialConversationState, action) {
 
     case SET_RECORDED_TEXT:
       return Object.assign({}, state, {recordedText: action.text})
+
+    case SET_CONVO_START_TIME:
+      return Object.assign({}, state, {startTime: action.time})
+
+    case SET_CONVO_END_TIME:
+      return Object.assign({}, state, {endTime: action.time})
 
     default:
       return state
