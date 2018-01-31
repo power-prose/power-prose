de4e6261bead80f187143c47496e48f6225dc2ce
Jacquelyn Wax <jacquelynwax@gmail.com>
2018-01-31 14:41:31 -0500

    provided material ui to application

--- client/components/AllConversationsView.js
+++ client/components/AllConversationsView.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import {AllConversationsData, WatchWords} from './';
-
-
+import RaisedButton from 'material-ui/RaisedButton';
 
 const AllConversationsView = (props) => {
 
   return (
     <div className="container-horizontal">
         <WatchWords />
+  <RaisedButton label="Default" />        
     <div className="container-right container-vertical">
     <AllConversationsData />
     </div>
--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
-//import Routes from './routes'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import { UserInterface } from './components'
 import { Router } from 'react-router-dom'
@@ -13,9 +13,11 @@ import './socket'
 
 ReactDOM.render(
   <Provider store={store}>
+    <MuiThemeProvider>
     <Router history={history}>
       <UserInterface />
     </Router>
+  </MuiThemeProvider>
   </Provider>,
   document.getElementById('app')
 )
