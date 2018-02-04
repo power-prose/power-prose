9aea99ffd897425eda20400e074ba5d1613ffae1
Ann Layman <annelayman@gmail.com>
2018-02-04 12:25:23 -0500

    minor improvements to landing page and added route for /about if we want to link loggedin users to landing page later perhaps in footer

--- client/components/UserInterface.js
+++ client/components/UserInterface.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { withRouter, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
-import { AllConversationsCarousel, LandingPage, Login, Signup, NavBar, LoggedOutHome, LoggedInHome, Footer } from "./";
+import { AllConversationsCarousel, LandingPage, Login, Signup, NavBar, LoggedInHome, Footer } from "./";
 import { me } from "../store";
 import RecordButtons from "./RecordButtons";
 
@@ -18,14 +18,15 @@ class UserInterface extends Component {
         <div className="container-horizontal">
           <Switch>
             {/* Routes placed here are available to all visitors */}
+            <Route exact path="/about" component={LandingPage} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
 
             {isLoggedIn && <Route path="/" component={LoggedInHome} />}
+
             <Route path="/" component={LandingPage} />
             {/* Displays our Login component as a fallback */}
             <Route component={Login} />
-
           </Switch>
         </div>
         <Footer />
