08072aa751cd8f50fc920bbb12f1b13e1858b20d
Hannah Weber <hkwweber@gmail.com>
2018-02-05 14:45:36 -0500

    commit it

--- client/components/WatchWordList.js
+++ client/components/WatchWordList.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import { updateUserWatchWord, postUserWatchWord, updateUser } from '../store';
-import { Avatar, Card, CardHeader, CardText, Chip, Dialog, Divider, FlatButton, FontIcon, RaisedButton, TextField } from 'material-ui';
-import { dateParser2 } from '../utils';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { updateUserWatchWord, postUserWatchWord, updateUser } from "../store";
+import {
+  Avatar,
+  Card,
+  CardHeader,
+  CardTitle,
+  CardText,
+  Chip,
+  Dialog,
+  Divider,
+  FlatButton,
+  FontIcon,
+  TextField
+} from "material-ui";
+import { dateParser2 } from "../utils";
 
 export class WatchWordList extends Component {
   constructor(props) {
     super(props);
-    this.state = { newWatchWord: '', dialogOpen: false }
+    this.state = { newWatchWord: "", dialogOpen: false };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     const { user } = this.props;
 
     if (user.acceptedInitialWatchWords === false) {
@@ -19,34 +31,34 @@ export class WatchWordList extends Component {
     }
   }
 
-  handleFormChange = (event) => {
+  handleFormChange = event => {
     this.setState({
       newWatchWord: event.target.value
     });
   };
 
   handleRequestDelete = () => {
-    alert('You clicked the delete button.');
-  }
+    alert("You clicked the delete button.");
+  };
 
   handleChipClick = () => {
-    alert('You clicked the Chip.');
-  }
+    alert("You clicked the Chip.");
+  };
 
   handleDialogOpen = () => {
-    this.setState({dialogOpen: true});
+    this.setState({ dialogOpen: true });
   };
 
   handleDialogClose = () => {
     const { user } = this.props;
     const userId = user.id;
     const request = { acceptedInitialWatchWords: true };
-    console.log('userId and request', userId, request)
+    console.log("userId and request", userId, request);
     this.setState({ dialogOpen: false });
-    this.props.updateUser(userId, request)
+    this.props.updateUser(userId, request);
   };
 
-  render () {
+  render() {
     const { watchWords, user } = this.props;
     const { newWatchWord } = this.state;
     const dialogAction = [
@@ -59,98 +71,122 @@ export class WatchWordList extends Component {
 
     return (
       <div className="container-watchwords">
-      <Card style={styles.cardStyle}>
-        <CardHeader title="Your WatchWords" />
-          {
-            watchWords.length && watchWords.map(word => (
-              <div key={word.id} style={styles.wrapper}>
-                <Chip
-                  onRequestDelete={(event) => this.props.onDeleteClick(event, word.id)}
-                  onClick={this.handleChipClick}
-                  style={styles.chip}
-                >
-                  {word.wordOrPhrase}
-                </Chip>
-              </div>
-            ))
-          }
+        <Card style={{width: "270px"}}>
+          <CardTitle
+          title="Your WatchWords"
+          style={{backgroundColor: "#DBF3E3"}}
+          titleStyle={{ "fontFamily": "Amaranth, sans-serif", "fontWeight": "bold"}}
+          titleColor="#0E254C"
+          />
+
+          <CardText>
+            {watchWords.length &&
+              watchWords.map(word => (
+                <div key={word.id} style={styles.wrapper}>
+                  <Chip
+
+                    labelStyle={{ color: "#0e254c" }}
+                    backgroundColor="#FFFFFF"
+                    onRequestDelete={event =>
+                      this.props.onDeleteClick(event, word.id)
+                    }
+                    onClick={this.handleChipClick}
+                    style={{
+                      marginBottom: 10,
+                      border: "0.5px solid #0e254c"
+                    }}
+                  >
+                    {word.wordOrPhrase}
+                  </Chip>
+                </div>
+              ))}
+
           <br />
-          <form style={styles.textFieldStyle} onSubmit={this.props.handleSubmit}>
+          <form
+            style={styles.textFieldStyle}
+            onSubmit={this.props.handleSubmit}
+          >
             <TextField
-              style={styles.textFieldStyle}
-              name='newwatchword'
-              hintText='Enter a new watchword'
+              style={{marginBottom: '15px'}}
+              inputStyle={{color: "#0E254C"}}
+              underlineFocusStyle={{ borderColor: "#C98E34"}}
+              floatingLabelFocusStyle={{color: "#C98E34"}}
+              fullWidth={true}
+              floatingLabelText="Enter a new watchword"
+              name="newwatchword"
               value={newWatchWord}
               onChange={this.handleFormChange}
-            /><br />
-            <RaisedButton label="Submit" secondary={true} style={styles.buttonStyle} />
+            />
+            <br />
+            <FlatButton
+              label="Submit"
+              secondary={true}
+              style={{
+                marginBottom: "5px",
+                color: "#0e254c",
+                border: "1px solid #0e254c"
+              }}
+              backgroundColor="#f0ddd4"
+              hoverColor="rgb(204,242,218)"
+              keyboardFocused={false}
+            />
           </form>
+          </CardText>
         </Card>
+
+
+
         <div>
-        <Dialog
-          title="Get Started with Watch Words"
-          actions={dialogAction}
-          modal={false}
-          open={this.state.dialogOpen}
-          onRequestClose={this.handleDialogClose}
-        >
-          Welcome! We have preloaded your account with a set of suggested watch words. Please use the sidebar to the left to personalize your list.
-        </Dialog>
+          <Dialog
+            title="Get Started with Watch Words"
+            actions={dialogAction}
+            modal={false}
+            open={this.state.dialogOpen}
+            onRequestClose={this.handleDialogClose}
+          >
+            Welcome! We have preloaded your account with a set of suggested
+            watch words. Please use the sidebar to the left to personalize your
+            list.
+          </Dialog>
         </div>
       </div>
-    )
+    );
   }
-};
+}
 
-
-const mapState = (state) => {
+const mapState = state => {
   return {
     watchWords: state.watchWords.activeWatchWords,
     user: state.user
-  }
+  };
 };
 
-const mapDispatch = (dispatch) => {
+const mapDispatch = dispatch => {
   return {
     onDeleteClick(event, wordId) {
       event.preventDefault();
-      dispatch(updateUserWatchWord(wordId))
+      dispatch(updateUserWatchWord(wordId));
     },
     handleSubmit(event) {
       event.preventDefault();
-      console.log('!!!!!', event.target.newwatchword.value)
-      let wordOrPhrase = event.target.newwatchword.value
-      dispatch(postUserWatchWord({ wordOrPhrase }))
+      console.log("!!!!!", event.target.newwatchword.value);
+      let wordOrPhrase = event.target.newwatchword.value;
+      dispatch(postUserWatchWord({ wordOrPhrase }));
     },
     updateUser(userId, request) {
-      dispatch(updateUser(userId, request))
+      dispatch(updateUser(userId, request));
     }
-  }
-}
+  };
+};
 
 export default withRouter(connect(mapState, mapDispatch)(WatchWordList));
 
 const styles = {
-  chip: {
-    margin: 10,
-  },
   wrapper: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textFieldStyle: {
-    margin: -4
-  },
-  cardStyle: {
-    maxWidth: 260
-  },
-  buttonStyle: {
-    margin: 12
+    display: "flex",
+    flexWrap: "wrap"
   }
 };
-
-
-
 
 // import React from 'react';
 // import Avatar from 'material-ui/Avatar';
