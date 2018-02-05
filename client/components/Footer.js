aa118e1b665fc0bf5b66c2e84186c513d6724a26
Hannah Weber <hkwweber@gmail.com>
2018-02-04 19:01:11 -0500

    update nav, update footer, style login signup comp

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 
 const Footer = props => {
   return (
     <nav className="navbar sticky-bottom container-horizontal footer  navbar-light bg-light">
-      <p>I am the Footer component. I am static and probably just a colored bar with no text.</p>
+    <div className="container-vertical footer-inner">
+      <div>Power Prose</div>
+      <a className="footer-link" href="/about">About</a>
+      </div>
           </nav>
   );
 };
